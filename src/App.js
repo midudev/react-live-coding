be2bc48f26a139888c9997e203c05553ac6ca585
midudev <miduga@gmail.com>
2020-04-24 19:23:46 +0200

    Add new routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Home from './pages/Home'
+import SearchResults from './pages/SearchResults'
 
-function App() {
+import { Link, Route } from "wouter"
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        <Link to="/">
+          <img className="App-logo" alt='Giffy logo' src='/logo.png' />
+        </Link>
+        <Route
+          component={Home}
+          path="/"
+        />
+        <Route
+          component={SearchResults}
+          path="/gif/:keyword"  />
+      </section>
     </div>
-  );
+  )
 }
-
-export default App;
