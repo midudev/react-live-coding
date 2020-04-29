42472177e84161667dbc70660c6ebb43ebc36464
Fernando Alejandro Yerena Ramos <ferdinandalexandros@gmail.com>
2020-04-29 18:56:19 -0500

    Category styles was created and App background was changed

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import {Link} from 'wouter'
 
+import './Category.css'
+
 export default function Category({name, options = []}){
-    return <div>
-        <h3 className="App-title">{name}</h3>
-        <ul>
+    return <div className="">
+        <h3 className="Category-title">{name}</h3>
+        <ul className="Category-list">
             {options.map((singleOption) => (
                 <li key={singleOption}>
-                    <Link to={`/search/${singleOption}`}>{singleOption}</Link>
+                    <Link 
+                        className="Category-link"
+                        to={`/search/${singleOption}`}
+                    >
+                        {singleOption}
+                    </Link>
                 </li>
             ))}
         </ul>
