b7749df6cd872aa4d5f82bc184c2876ac366394f
midudev <miduga@gmail.com>
2020-05-08 16:33:40 +0200

    Fix Category styles and props

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -3,9 +3,9 @@ import { Link } from "wouter";
 
 import "./Category.css";
 
-export default function Category({ name, options = [], ...props }) {
+export default function Category({ name, options = [] }) {
   return (
-    <div className={props.className}>
+    <div className='Category'>
       <h3 className="Category-title">{name}</h3>
       <ul className="Category-list">
         {options.map((singleOption) => (
