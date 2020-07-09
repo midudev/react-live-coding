626c026b2880d577ab406e7f1fb7b16d96896746
midudev <miduga@gmail.com>
2020-07-09 22:53:21 +0200

    Add emotion to project

--- src/pages/ErrorPage/index.js
+++ src/pages/ErrorPage/index.js
@@ -1,14 +1,51 @@
-import React from "react";
+/** @jsx jsx */
+/** @jsxFrag React.Fragment */
+import { jsx, css } from '@emotion/core'
+
+import React from 'react'
 import SearchForm from "components/SearchForm";
 import { Helmet } from "react-helmet";
 import { Link } from "wouter";
 
-const gifsErrors = ['d2jjuAZzDSVLZ5kI', 'Bp3dFfoqpCKFyXuSzP', 'hv5AEBpH3ZyNoRnABG', 'hLwSzlKN8Fi6I'];
+const gifsErrors = [
+  "d2jjuAZzDSVLZ5kI",
+  "Bp3dFfoqpCKFyXuSzP",
+  "hv5AEBpH3ZyNoRnABG",
+  "hLwSzlKN8Fi6I",
+];
+
+const pageErrorStyles = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  text-align: center;
+`
+
+const codeErrorStyles = css`
+font-size: 5rem;
+font-weight: bold;
+font-style: italic;
+`
+
+const msgErrorStyles = css`
+  font-size: 1.5rem;
+  margin: 1rem 0;
+`
+
+const gifErrorStyles = css`
+margin: 1rem auto;
+width: 250px;
+height: 250px;
+`
 
 export default function ErrorPage() {
   const randomImage = () => {
-    return `https://media.giphy.com/media/${gifsErrors[Math.floor(Math.random() * gifsErrors.length) + 1 ]}/giphy.gif`
-  }
+    return `https://media.giphy.com/media/${
+      gifsErrors[Math.floor(Math.random() * gifsErrors.length) + 1]
+    }/giphy.gif`;
+  };
 
   return (
     <>
@@ -18,13 +55,13 @@ export default function ErrorPage() {
       <header className="o-header">
         <SearchForm />
       </header>
-      <div className="App-wrapper">
-        <div className="App-main page-error">
-            <span class="code-error">404</span>
-            <span class="msg-error">Sometimes gettings lost isn't that bad</span>
-            <img class="gif-error" src={randomImage()} alt="alt-page-404"/>
-            <Link href='/' class="btn">Go to home</Link>
-        </div>
+      <div css={pageErrorStyles}>
+        <span css={codeErrorStyles}>404</span>
+        <span class={msgErrorStyles}>Sometimes gettings lost isn't that bad</span>
+        <img class={gifErrorStyles} src={randomImage()} alt="alt-page-404" />
+        <Link href="/" class="btn">
+          Go to home
+        </Link>
       </div>
     </>
   );
